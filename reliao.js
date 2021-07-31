8cdd090f25721aa8e63b9a241eeaa5d115557bf4
saltedyu <86279057@qq.com>
2021-07-31 15:01:08 +0800

    Update reliao.js

--- reliao.js
+++ reliao.js
@@ -1,9 +1,16 @@
 
-var body = $request.body;
-body['longitude'] = '116.288160';
-body['latitude'] = '39.177440';
+//var body = $request.body;
+//body['longitude'] = '116.288160';
+//body['latitude'] = '39.177440';
 
-body = JSON.stringify(body)
-  $done({
-    body
-  })
+//body = JSON.stringify(body)
+//  $done({
+//    body
+//  })
+
+let body = JSON.parse($response.body)
+body.longitude = '116.288160';
+body.latitude ='39.177440';
+
+body = JSON.stringify(body);
+$done({body});
