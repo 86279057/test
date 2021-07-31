d02e8c534e5b9a0bd85d90f35fddb4e402b89240
saltedyu <86279057@qq.com>
2021-07-31 16:32:50 +0800

    Update reliao.js

--- reliao.js
+++ reliao.js
@@ -9,8 +9,14 @@
 //  })
 
 let body = JSON.parse($request.body);
-body.longitude = '116.288160';
-body.latitude ='39.177440';
+
+last_longitude = Math.random()
+last_latitude = Math.random()
+fist_longitude 
+body.longitude = '116.32005';
+body.latitude ='39.989571';
+
+
 
 body = JSON.stringify(body);
 console.log(body);
